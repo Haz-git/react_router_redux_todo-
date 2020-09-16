8046ac6cdbf62d59f3d3448b71c9537036eca560
Haz-git <zhouharry1@gmail.com>
2020-09-16 05:41:55 -0700

    Setting up for react-router

---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div className='ui clearing segment'>
+            <div className='ui right floated header'>
+                Sign In
+            </div>
+            <div className='ui left floated header'>
+                TodoApp
+            </div>
+        </div>
+    )
+}
+
+export default Header;
