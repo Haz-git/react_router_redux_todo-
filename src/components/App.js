fe6eb47fdc61cb317fe5c13822fbf4970d000941
Haz-git <zhouharry1@gmail.com>
2020-09-16 05:51:26 -0700

    Finished Connecting React Router

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import Header from './Header';
+import SignIn from './SignIn';
+import TodoApp from './TodoApp';
+import LandingPage from './LandingPage';
+import { BrowserRouter, Route } from 'react-router-dom';
+
+
 
 const App = () => {
     return (
         <div className='ui container'>
-            <Header />
+            <BrowserRouter>
+                <Header />
+                <Route path='/' exact component={LandingPage} />
+                <Route path='/signin' exact component={SignIn} />
+                <Route path='/todo' exact component={TodoApp} />
+            </BrowserRouter>
         </div>
     )
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,17 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
     return (
         <div className='ui clearing segment'>
             <div className='ui right floated header'>
-                Sign In
+                <Link to='/signin'>Sign In</Link>
+            </div>
+            <div className='ui center floated header'>
+                <Link to='/'>Home</Link>
             </div>
             <div className='ui left floated header'>
-                TodoApp
+                <Link to='/todo'>TodoApp</Link>
             </div>
         </div>
     )
